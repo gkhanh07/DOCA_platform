f5ac2af4f9a20c4b5c84ec270c0fb9d213762201
albuszin <thientpse173447@fpt.edu.vn>
2023-10-14 06:15:32 +0700

    logingmailtest

---
+++ LoginGmailTest/src/main/java/com/controller/Constants.java
@@ -0,0 +1,26 @@
+package com.controller;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author heaty566
+ */
+public class Constants {
+
+	public static String GOOGLE_CLIENT_ID = "1064043187978-lkhbeshhf0i9hkoqbi30g48tt002klqu.apps.googleusercontent.com";
+
+	public static String GOOGLE_CLIENT_SECRET = "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";
+
+	public static String GOOGLE_REDIRECT_URI = "http://localhost:8084/Logingmailtest/LoginGoogleHandler";
+
+	public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+
+	public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+
+	public static String GOOGLE_GRANT_TYPE = "authorization_code";
+
+}
---
+++ LoginGmailTest/target/Logingmailtest-1.0-SNAPSHOT/login.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : login
+    Created on : Jul 1, 2021, 1:12:31 PM
+    Author     : heaty566
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+        <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+                <title>JSP Page</title>
+        </head>
+        <body>
+                <a href="https://accounts.google.com/o/oauth2/auth?scope=profile&redirect_uri=http://localhost:8084/Logingmailtest/LoginGoogleHandler&response_type=code
+		   &client_id=1064043187978-lkhbeshhf0i9hkoqbi30g48tt002klqu.apps.googleusercontent.com&approval_prompt=force">Login With Google</a>
+        </body>
+</html>
