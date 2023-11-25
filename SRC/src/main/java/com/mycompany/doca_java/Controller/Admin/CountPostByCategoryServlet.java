903f67c8b1ee94bc8ec7e74dd0f2e3984d79981a
laptop-m8du3f9f <quangtran214203@gmail.com>
2023-11-26 01:13:55 +0700

    add more chart to admin page

--- SRC/src/main/java/com/mycompany/doca_java/Controller/Admin/CountPostByCategoryServlet.java
+++ SRC/src/main/java/com/mycompany/doca_java/Controller/Admin/CountPostByCategoryServlet.java
@@ -4,14 +4,9 @@
  */
 package com.mycompany.doca_java.Controller.Admin;
 
-import com.mycompany.doca_java.DAO.dateDAO;
-import com.mycompany.doca_java.DAO.userDAO;
-import com.mycompany.doca_java.DAO.weekDAO;
-import com.mycompany.doca_java.DTO.dateDTO;
-import com.mycompany.doca_java.DTO.userDTO;
-import com.mycompany.doca_java.DTO.weekDTO;
+import com.mycompany.doca_java.DAO.categoryDAO;
+import com.mycompany.doca_java.DTO.categoryDTO;
 import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletContext;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -19,20 +14,20 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.naming.NamingException;
 
 /**
  *
  * @author Admin
  */
-@WebServlet(name = "manageCalendar", urlPatterns = {"/manageCalendar"})
-public class manageCalendar extends HttpServlet {
+@WebServlet(name = "CountPostByCategoryServlet", urlPatterns = {"/CountPostByCategoryServlet"})
+public class CountPostByCategoryServlet extends HttpServlet {
 
-    private final String manageCelendarJsp = "AdminUI/manageCelendar.jsp";
+    private final String CircleChartPage = "AdminUI/circleChart.jsp";
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -45,43 +40,24 @@ public class manageCalendar extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        HttpSession session = request.getSession();
-        ServletContext context = getServletContext();
         String url = "";
         try {
-            weekDAO wDao = new weekDAO();
-            LocalDate currentDate = java.time.LocalDate.now();
-            wDao.getAllTheWeek();
-            List<weekDTO> ListOfWeek = wDao.getListOfWeek();
-            for (weekDTO week : ListOfWeek) {
-                if (currentDate.isEqual(week.getStartDate()) || currentDate.isEqual(week.getEndDate())
-                        || (currentDate.isAfter(week.getStartDate()) && currentDate.isBefore(week.getEndDate()))) {
-                    String selectedWeek = week.getStartDate() + "/" + week.getEndDate();
-                    url = "MangeDateInWeek?selectedWeek=" + selectedWeek;
-//                    LocalDate inWeek = week.getStartDate();
-//                    dateDAO dDao = new dateDAO();
-//                    List<dateDTO> listDateInWeek = dDao.getWeeksInRange(week.getStartDate(), week.getEndDate());
-//                    if (listDateInWeek != null) {
-//                        context.setAttribute("listDateInWeek", listDateInWeek);
-//                    }
-//                    request.setAttribute("inWeek", inWeek.toString());
-                }
+            response.setContentType("text/html;charset=UTF-8");
+            categoryDAO dao = new categoryDAO();
+            List<categoryDTO> list = dao.getCountPostByCategory();
+            if (list != null) {
+                request.setAttribute("ListPercentagePostByCategory", list);
+                url = CircleChartPage;
             }
-            userDAO udao = new userDAO();
-            List<userDTO> listAdmin = udao.getUsersByRoleID(0);
-            session.setAttribute("listAdmin", listAdmin);
-            session.setAttribute("ListOfWeek", ListOfWeek);
-            // session.setAttribute("currentDate", currentDate);
-        } catch (ClassNotFoundException ex) {
-            ex.printStackTrace();
-        } catch (NamingException ex) {
-            ex.printStackTrace();
         } catch (SQLException ex) {
-            ex.printStackTrace();
+            Logger.getLogger(CountPostByCategoryServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(CountPostByCategoryServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NamingException ex) {
+            Logger.getLogger(CountPostByCategoryServlet.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
-            RequestDispatcher dispatcher = request.getRequestDispatcher(url);
-            dispatcher.forward(request, response);
+            RequestDispatcher rd = request.getRequestDispatcher(url);
+            rd.forward(request, response);
         }
     }
 
